3f3b976d33638f5833816a160e53cd0bf11362a1
lnash94 <sumudunissanke@gmail.com>
2025-11-18 12:54:29 +0530

    Fix review suggestions

--- src/main/java/org/wso2/mi/tool/connector/tools/generator/grpc/model/RPCService.java
+++ src/main/java/org/wso2/mi/tool/connector/tools/generator/grpc/model/RPCService.java
@@ -46,7 +46,6 @@ public class RPCService {
     public static class RPCCall {
         private final String rpcCallName;
         private final String inputName;
-        private final String reqClassName;
         private final String outputName;
         private final Map<String, DescriptorProtos.FieldDescriptorProto> input;
         private final Map<String, DescriptorProtos.FieldDescriptorProto> output;
@@ -59,7 +58,6 @@ public class RPCService {
             this.input = builder.input;
             this.output = builder.output;
             this.comment = builder.comment;
-            this.reqClassName = builder.reqClassName;
         }
 
         public String getRpcCallName() {
@@ -85,9 +83,6 @@ public class RPCService {
         public String getComment() {
             return comment;
         }
-        public String getReqClassName() {
-            return reqClassName;
-        }
 
         public static class RPCCallBuilder {
             private String rpcCallName;
@@ -97,8 +92,6 @@ public class RPCService {
             private Map<String, DescriptorProtos.FieldDescriptorProto> output = new HashMap<>();
 
             private String comment = "";
-            private String reqClassName;
-
             public RPCCallBuilder rpcCallName(String rpcCallName) {
                 this.rpcCallName = rpcCallName;
                 return this;
@@ -147,11 +140,6 @@ public class RPCService {
 
             public RPCCallBuilder comment(String comment) {
                 this.comment = comment;
-                return this;
-            }
-
-            public RPCCallBuilder reqClassName(String reqClassName) {
-                this.reqClassName = reqClassName;
                 return this;
             }
 
